debd7bef6a8424a90a0874327a518c0064e00d35
anolol <anoemangi@gmail.com>
2019-09-22 20:01:01 -0300

    Teste de Windows Serviçe foi com sucesso

---
+++ AppWindonws/ConfigureService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace AppWindonws
+{
+    public class ConfigureService
+    {
+        internal static void Configure()
+        {
+            HostFactory.Run(configure =>
+            {
+                configure.Service<MeuWindowsService>(service =>
+                {
+                    service.ConstructUsing(s => new MeuWindowsService());
+                    service.WhenStarted(s => s.Start());
+                    service.WhenStopped(s => s.Stop());
+                });
+                configure.RunAsLocalSystem();
+                configure.SetServiceName("MeuServicoWindowsComTopshelf");
+                configure.SetDisplayName("MeuServicoWindowsComTopshelf");
+                configure.SetDescription("Meu serviço Windows com Topshelf");
+            });
+        }
+    }
+}
---
+++ AppWindonws/MeuWindowsService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace AppWindonws
+{
+    public class MeuWindowsService
+    {
+        public void Start()
+        {
+            var timer1 = new Timer();
+            timer1.Interval = 10000;
+            timer1.Elapsed += new System.Timers.ElapsedEventHandler(timer1_Tick);
+            timer1.Enabled = true;
+            Console.WriteLine("Meu Serviço do Windows foi iniciado");
+
+        }
+
+        private void timer1_Tick(object sender,ElapsedEventArgs e)
+        {
+            Console.WriteLine("Meu Serviço do Windows sendo executado a cada 10 segundos ");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Meu Serviço do Windows Encerrou");
+        }
+    }
+}
